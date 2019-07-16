8b19f96eb7b78431f1dc0ec9bb56b90ad9516729
dorota <dorota.sawicki@gmail.com>
2019-07-16 17:36:45 -0600

    loader wip

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ import initGlobe from './lib/main';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    document.getElementById('loader');
-
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-
     initGlobe();
-
 });
 
